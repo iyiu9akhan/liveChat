54d35fb4517f723fed4ff34b0e6391b7059cd975
iyiu9akhan <iyiu9akhan@gmail.com>
2025-06-26 15:54:15 +0600

    live issue with gitHub pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./App.css";
 import App from "./App.jsx";
 import firebaseConfig from "./authentication/firebase.config.js";
+import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>
+  </BrowserRouter>
 );
