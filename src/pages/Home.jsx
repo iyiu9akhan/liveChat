6451d96be8d6c85f8c801e382a5286a2bb0e2b35
iyiu9akhan <iyiu9akhan@gmail.com>
2025-06-29 20:18:56 +0600

    windows: home page design solved

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,23 @@ import { IoSettingsSharp } from "react-icons/io5";
 import { FaSignOutAlt } from "react-icons/fa";
 import { IoSearch } from "react-icons/io5";
 import { PiDotsThreeOutlineVerticalFill } from "react-icons/pi";
+import { FaPlus } from "react-icons/fa";
+// import group_one from "../assets/home/groupList/group_one.png";
+// import group_two from "../assets/home/groupList/group_two.png";
+// import group_three from "../assets/home/groupList/group_three.png";
+// import friendRqst_one from "../assets/home/friendRqst/frdRqst_one.png";
+// import friendRqst_two from "../assets/home/friendRqst/frdRqst_two.png";
+// import friendRqst_three from "../assets/home/friendRqst/frdRqst_three.png";
+// import friendRqst_four from "../assets/home/friendRqst/frdRqst_four.png";
+// import HomeInfo from "./HomeInfo";
+import {
+  groupList,
+  friendRqst,
+  friends,
+  userList,
+  myGroup,
+  blockedUsers,
+} from "./User";
 
 function Home() {
   return (
@@ -14,29 +31,31 @@ function Home() {
       <Container>
         <div className="flex h-screen py-[35px]">
           <div className="w-[186px] h-full bg-primary rounded-[20px] flex flex-col  py-[38px] justify-between">
-            <div className="mb-[78px] flex justify-center">
-              <img
-                src={profilePic}
-                alt="#profile_pic"
-                className="h-[100px] w-[100px] rounded-full object-cover cursor-pointer"
-              />
-            </div>
-            <div className="flex flex-col items-end relative">
-              <div className="relative cursor-pointer h-[89px] text-[#C3C3C3] hover:bg-white hover:text-[#1e1e1e] duration-300 mb-[26px] w-[161px] rounded-l-[20px] flex items-center pl-[45px] group">
-                <GoHomeFill size={46} />
-                <div className="absolute w-[8px] h-[89px] bg-[#1e1e1e] top-0 right-0 rounded-l-[25px] group-hover:shadow-[-2px_0px_4px_0px_rgba(0,0,0,0.25)] duration-300"></div>
+            <div>
+              <div className="mb-[78px] flex justify-center">
+                <img
+                  src={profilePic}
+                  alt="#profile_pic"
+                  className="h-[100px] w-[100px] rounded-full object-cover cursor-pointer"
+                />
               </div>
-              <div className="relative cursor-pointer h-[89px] text-[#C3C3C3] hover:bg-white hover:text-[#1e1e1e] duration-300 mb-[26px] w-[161px] rounded-l-[20px] flex items-center pl-[45px] group">
-                <AiFillMessage size={46} />
-                <div className="absolute w-[8px] h-[89px] bg-[#1e1e1e] top-0 right-0 rounded-l-[25px] group-hover:shadow-[-2px_0px_4px_0px_rgba(0,0,0,0.25)] duration-300"></div>
-              </div>
-              <div className="relative cursor-pointer h-[89px] text-[#C3C3C3] hover:bg-white hover:text-[#1e1e1e] duration-300 mb-[26px] w-[161px] rounded-l-[20px] flex items-center pl-[45px] group">
-                <IoSettingsSharp size={46} />
-                <div className="absolute w-[8px] h-[89px] bg-[#1e1e1e] top-0 right-0 rounded-l-[25px] group-hover:shadow-[-2px_0px_4px_0px_rgba(0,0,0,0.25)] duration-300"></div>
+              <div className="flex flex-col items-end relative">
+                <div className="relative cursor-pointer h-[89px] text-[#C3C3C3] hover:bg-white hover:text-[#1e1e1e] duration-300 mb-[26px] w-[161px] rounded-l-[20px] flex items-center pl-[45px] group">
+                  <GoHomeFill size={46} />
+                  <div className="absolute w-[8px] h-[89px] bg-[#1e1e1e] top-0 right-0 rounded-l-[25px] group-hover:shadow-[-2px_0px_4px_0px_rgba(0,0,0,0.25)] duration-300"></div>
+                </div>
+                <div className="relative cursor-pointer h-[89px] text-[#C3C3C3] hover:bg-white hover:text-[#1e1e1e] duration-300 mb-[26px] w-[161px] rounded-l-[20px] flex items-center pl-[45px] group">
+                  <AiFillMessage size={46} />
+                  <div className="absolute w-[8px] h-[89px] bg-[#1e1e1e] top-0 right-0 rounded-l-[25px] group-hover:shadow-[-2px_0px_4px_0px_rgba(0,0,0,0.25)] duration-300"></div>
+                </div>
+                <div className="relative cursor-pointer h-[89px] text-[#C3C3C3] hover:bg-white hover:text-[#1e1e1e] duration-300 mb-[26px] w-[161px] rounded-l-[20px] flex items-center pl-[45px] group">
+                  <IoSettingsSharp size={46} />
+                  <div className="absolute w-[8px] h-[89px] bg-[#1e1e1e] top-0 right-0 rounded-l-[25px] group-hover:shadow-[-2px_0px_4px_0px_rgba(0,0,0,0.25)] duration-300"></div>
+                </div>
               </div>
             </div>
             <div className="flex items-end flex-col">
-              <div className="relative cursor-pointer h-[89px] text-[#C3C3C3] hover:bg-white hover:text-[#1e1e1e] duration-300 mb-[26px] w-[161px] rounded-l-[20px] flex items-center pl-[45px] group">
+              <div className="relative cursor-pointer h-[89px] text-[#ffffff] hover:bg-white hover:text-[#1e1e1e] duration-300 mb-[26px] w-[161px] rounded-l-[20px] flex items-center pl-[45px] group">
                 <FaSignOutAlt size={46} />
                 <div className="absolute w-[8px] h-[89px] bg-[#1e1e1e] top-0 right-0 rounded-l-[25px] group-hover:shadow-[-2px_0px_4px_0px_rgba(0,0,0,0.25)] duration-300"></div>
               </div>
@@ -69,6 +88,31 @@ function Home() {
                     className="absolute right-[23px] top-[20px] cursor-pointer mt-1"
                     size={20}
                   />
+                  <div>
+                    {groupList.map((group, index) => (
+                      <div
+                        key={index}
+                        className="flex items-center mt-[17px] justify-between"
+                      >
+                        <div className="flex items-center">
+                          <img src={group.img} alt="#group_img" />
+                          <div className="mx-[14px] ">
+                            <h1 className="capitalize font-regular text-[18px] text-black font-semibold">
+                              {group.title}
+                            </h1>
+                            <p className="font-regular font-medium text-[14px] text-[#4D4D4D]">
+                              {group.subtitle}
+                            </p>
+                          </div>
+                        </div>
+                        <div className="bg-primary rounded-[5px] h-[30px] w-[87px] flex justify-center items-center cursor-pointer mr-[9px]">
+                          <p className="capitalize cursor-pointer text-white font-regular font-semibold text-[20px]">
+                            join
+                          </p>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
                 </div>
               </div>
 
@@ -81,6 +125,31 @@ function Home() {
                     className="absolute right-[23px] top-[20px] cursor-pointer mt-1"
                     size={20}
                   />
+                  <div>
+                    {friendRqst.map((friendRqst, index) => (
+                      <div
+                        key={index}
+                        className="flex items-center mt-[17px] justify-between"
+                      >
+                        <div className="flex items-center">
+                          <img src={friendRqst.img} alt="#profile_img" />
+                          <div className="mx-[14px]">
+                            <h1 className="capitalize font-regular text-[18px] text-black font-semibold">
+                              {friendRqst.title}
+                            </h1>
+                            <p className="font-regular font-medium text-[14px] text-[#4D4D4D] capitalize">
+                              {friendRqst.subtitle}
+                            </p>
+                          </div>
+                        </div>
+                        <div className="bg-primary rounded-[5px] h-[30px] w-[87px] flex justify-center items-center cursor-pointer mr-[9px]">
+                          <p className="capitalize cursor-pointer text-white font-regular font-semibold text-[16px]">
+                            Accept
+                          </p>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
                 </div>
               </div>
             </div>
@@ -95,6 +164,29 @@ function Home() {
                     className="absolute right-[23px] top-[20px] cursor-pointer mt-2"
                     size={20}
                   />
+                  <div>
+                    {friends.map((friends, index) => (
+                      <div
+                        key={index}
+                        className="flex items-center mt-[17px] justify-between"
+                      >
+                        <div className="flex items-center">
+                          <img src={friends.img} alt="#friend_img" />
+                          <div className="mx-[14px] ">
+                            <h1 className="capitalize font-regular text-[14px] text-black font-semibold">
+                              {friends.title}
+                            </h1>
+                            <p className="font-regular font-medium text-[12px] text-[#4D4D4D] capitalize">
+                              {friends.subtitle}
+                            </p>
+                          </div>
+                        </div>
+                        <p className="capitalize text-black/50 font-regular font-medium text-[10px] mr-[9px]">
+                          {friends.time}
+                        </p>
+                      </div>
+                    ))}
+                  </div>
                 </div>{" "}
                 <div className="w-[344px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] rounded-[20px] p-[22px] relative">
                   <h1 className="capitalize font-regular font-semibold text-[20px] text-black ">
@@ -104,17 +196,63 @@ function Home() {
                     className="absolute right-[23px] top-[20px] cursor-pointer mt-2"
                     size={20}
                   />
+                  <div>
+                    {userList.map((userList, index) => (
+                      <div
+                        key={index}
+                        className="flex items-center mt-[17px] justify-between"
+                      >
+                        <div className="flex items-center">
+                          <img src={userList.img} alt="#friend_img" />
+                          <div className="mx-[14px] ">
+                            <h1 className="capitalize font-regular text-[14px] text-black font-semibold ">
+                              {userList.title}
+                            </h1>
+                            <p className="font-regular font-medium text-[12px] text-[#4D4D4D] capitalize">
+                              {userList.time}
+                            </p>
+                          </div>
+                        </div>
+                        <div className="h-[30px] w-[30px] rounded-[5px] bg-primary flex items-center justify-center cursor-pointer mr-[9px]">
+                          <FaPlus className="text-white" size={14} />
+                        </div>
+                      </div>
+                    ))}
+                  </div>
                 </div>
               </div>
               <div className="flex gap-[19px] h-[50%]">
                 <div className="w-[344px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] rounded-[20px] p-[22px]  relative">
-                  <h1 className="capitalize font-regular font-semibold text-[20px] text-black ">
+                  <h1 className="capitalize font-regular font-semibold text-[20px] text-black">
                     my groups
                   </h1>
                   <PiDotsThreeOutlineVerticalFill
                     className="absolute right-[23px] top-[20px] cursor-pointer mt-2"
                     size={20}
                   />
+                  <div>
+                    {myGroup.map((myGroup, index) => (
+                      <div
+                        key={index}
+                        className="flex items-center mt-[17px] justify-between"
+                      >
+                        <div className="flex items-center">
+                          <img src={myGroup.img} alt="#friend_img" />
+                          <div className="mx-[14px] ">
+                            <h1 className="capitalize font-regular text-[14px] text-black font-semibold ">
+                              {myGroup.title}
+                            </h1>
+                            <p className="font-regular font-medium text-[12px] text-[#4D4D4D] capitalize">
+                              {myGroup.subtitle}
+                            </p>
+                          </div>
+                        </div>
+                        <p className="capitalize text-black/50 font-regular font-medium text-[10px] mr-[9px]">
+                          {myGroup.time}
+                        </p>
+                      </div>
+                    ))}
+                  </div>
                 </div>
                 <div className="w-[344px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] rounded-[20px] p-[22px] relative ">
                   <h1 className="capitalize font-regular font-semibold text-[20px] text-black ">
@@ -124,6 +262,31 @@ function Home() {
                     className="absolute right-[23px] top-[20px] cursor-pointer mt-2"
                     size={20}
                   />
+                  <div>
+                    {blockedUsers.map((blockedUsers, index) => (
+                      <div
+                        key={index}
+                        className="flex items-center mt-[17px] justify-between"
+                      >
+                        <div className="flex items-center">
+                          <img src={blockedUsers.img} alt="#friend_img" />
+                          <div className="mx-[14px] ">
+                            <h1 className="capitalize font-regular text-[14px] text-black font-semibold ">
+                              {blockedUsers.title}
+                            </h1>
+                            <p className="font-regular font-medium text-[12px] text-[#4D4D4D] capitalize">
+                              {blockedUsers.time}
+                            </p>
+                          </div>
+                        </div>
+                        <div className="bg-primary rounded-[5px] h-[30px] w-[87px] flex justify-center items-center cursor-pointer mr-[9px]">
+                          <p className="capitalize cursor-pointer text-white font-regular font-semibold text-[16px]">
+                            unblock
+                          </p>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
                 </div>
               </div>
             </div>
---
+++ src/pages/User.jsx
@@ -0,0 +1,180 @@
+import group_one from "../assets/home/groupList/group_one.png";
+import group_two from "../assets/home/groupList/group_two.png";
+import group_three from "../assets/home/groupList/group_three.png";
+
+import friendRqst_one from "../assets/home/friendRqst/frdRqst_one.png";
+import friendRqst_two from "../assets/home/friendRqst/frdRqst_two.png";
+import friendRqst_three from "../assets/home/friendRqst/frdRqst_three.png";
+import friendRqst_four from "../assets/home/friendRqst/frdRqst_four.png";
+
+import friend_one from "../assets/home/friends/friend_one.png";
+import friend_two from "../assets/home/friends/friend_two.png";
+import friend_three from "../assets/home/friends/friend_three.png";
+import friend_four from "../assets/home/friends/friend_four.png";
+
+import user_one from "../assets/home/userList/user_one.png";
+import user_two from "../assets/home/userList/user_two.png";
+import user_three from "../assets/home/userList/user_three.png";
+import user_four from "../assets/home/userList/user_four.png";
+import user_five from "../assets/home/userList/user_five.png";
+
+import myGroup_one from "../assets/home/myGroup/myGroup_one.png";
+import myGroup_two from "../assets/home/myGroup/myGroup_two.png";
+import myGroup_three from "../assets/home/myGroup/myGroup_three.png";
+import myGroup_four from "../assets/home/myGroup/myGroup_four.png";
+
+import blockedUser_one from "../assets/home/blockedUsers/blockedUser_one.png";
+import blockedUser_two from "../assets/home/blockedUsers/blockedUser_two.png";
+import blockedUser_three from "../assets/home/blockedUsers/blockedUser_three.png";
+import blockedUser_four from "../assets/home/blockedUsers/blockedUser_four.png";
+import blockedUser_five from "../assets/home/blockedUsers/blockedUser_five.png";
+
+export const groupList = [
+  {
+    img: group_one,
+    title: "Friends Reunion",
+    subtitle: "Hi Guys, Wassup!",
+  },
+  {
+    img: group_two,
+    title: "Friends Forever",
+    subtitle: "Good to see you!",
+  },
+  {
+    img: group_three,
+    title: "Crazy Cousins",
+    subtitle: "What plans today?",
+  },
+];
+
+export const friendRqst = [
+  {
+    img: friendRqst_one,
+    title: "raghav",
+    subtitle: "dinner?",
+  },
+  {
+    img: friendRqst_two,
+    title: "swathi",
+    subtitle: "sure!",
+  },
+  {
+    img: friendRqst_three,
+    title: "kiran",
+    subtitle: "hi...",
+  },
+  {
+    img: friendRqst_four,
+    title: "Tejeshwini C",
+    subtitle: "i will call him today.",
+  },
+];
+
+export const friends = [
+  {
+    img: friend_one,
+    title: "raghav",
+    subtitle: "dinner?",
+    time: "Today, 8:56pm",
+  },
+  {
+    img: friend_two,
+    title: "swathi",
+    subtitle: "sure!",
+    time: "Today, 2:31pm",
+  },
+  {
+    img: friend_three,
+    title: "kiran",
+    subtitle: "hi...",
+    time: "Yesterday, 6:22pm",
+  },
+  {
+    img: friend_four,
+    title: "Tejeshwini C",
+    subtitle: "i will call him today.",
+    time: "Today, 12:22pm",
+  },
+];
+export const userList = [
+  {
+    img: user_one,
+    title: "raghav",
+    time: "Today, 8:56pm",
+  },
+  {
+    img: user_two,
+    title: "swathi",
+    time: "Today, 2:31pm",
+  },
+  {
+    img: user_three,
+    title: "kiran",
+    time: "Yesterday, 6:22pm",
+  },
+  {
+    img: user_four,
+    title: "Tejeshwini C",
+    time: "Today, 12:22pm",
+  },
+  {
+    img: user_five,
+    title: "Marvin McKinney",
+    time: "Today, 12:22pm",
+  },
+];
+
+export const myGroup = [
+  {
+    img: myGroup_one,
+    title: "raghav",
+    subtitle: "dinner?",
+    time: "Today, 8:56pm",
+  },
+  {
+    img: myGroup_two,
+    title: "swathi",
+    subtitle: "sure!",
+    time: "Today, 2:31pm",
+  },
+  {
+    img: myGroup_three,
+    title: "kiran",
+    subtitle: "hi...",
+    time: "Yesterday, 6:22pm",
+  },
+  {
+    img: myGroup_four,
+    title: "Tejeshwini C",
+    subtitle: "i will call him today.",
+    time: "Today, 12:22pm",
+  },
+];
+
+export const blockedUsers = [
+  {
+    img: blockedUser_one,
+    title: "raghav",
+    time: "Today, 8:56pm",
+  },
+  {
+    img: blockedUser_two,
+    title: "swathi",
+    time: "Today, 2:31pm",
+  },
+  {
+    img: blockedUser_three,
+    title: "kiran",
+    time: "Yesterday, 6:22pm",
+  },
+  {
+    img: blockedUser_four,
+    title: "Tejeshwini C",
+    time: "Today, 12:22pm",
+  },
+  {
+    img: blockedUser_five,
+    title: "Marvin ",
+    time: "Today, 8:56pm",
+  },
+];
