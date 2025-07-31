39afd696022cff14f61c5eca4a41941524c52222
iyiu9akhan <iyiu9akhan@gmail.com>
2025-07-31 20:24:14 +0600

    windows: createGroupPopUp

--- src/components/home/groupList/GroupList.jsx
+++ src/components/home/groupList/GroupList.jsx
@@ -9,7 +9,19 @@ import random_profile from "../../../assets/home/random_profile.jpg";
 function GroupList() {
   const [searchUser, setSearchUser] = useState([]);
   const [show, setShow] = useState(false);
+  const [groupName, setGroupName] = useState("");
+  const [groupNameErr, setGroupNameErr] = useState("");
+  const [shakeGroupName, setShakeGroupName] = useState(false);
 
+  const triggerShakeEmail = () => {
+    setShakeGroupName(false);
+    setTimeout(() => {
+      setShakeGroupName(true);
+    }, 10);
+  };
+  const handleAnimationEnd = () => {
+    setShakeGroupName(false);
+  };
   const searchHandler = (e) => {
     const value = e.target.value.toLowerCase();
     if (value === "") {
@@ -22,10 +34,24 @@ function GroupList() {
     }
   };
 
-  const CreateGroupHandler = () => {
+  const CreateGroupIconHandler = () => {
+    if (show) {
+      setGroupName("");
+      setGroupNameErr("");
+      // setShakeGroupName(false);
+    }
     setShow(!show);
   };
-
+  const handleGroupName = (e) => {
+    setGroupName(e.target.value);
+    setGroupNameErr("");
+  };
+  const handleCreateGroup = () => {
+    if (!groupName) {
+      setGroupNameErr("Group name is required !");
+      triggerShakeEmail();
+    }
+  };
   return (
     <div className="shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] rounded-[20px] p-[20px] pr-[10px] md:w-[427px] h-full relative">
       {/* <div className="flex justify-between pr-6">
@@ -54,7 +80,7 @@ function GroupList() {
       {show ? (
         <IoIosBackspace
           className="cursor-pointer text-[#EF4444]"
-          onClick={CreateGroupHandler}
+          onClick={CreateGroupIconHandler}
           size={35}
         />
       ) : (
@@ -66,7 +92,7 @@ function GroupList() {
             <Search onChange={searchHandler} />
             <div>
               <MdGroupAdd
-                onClick={CreateGroupHandler}
+                onClick={CreateGroupIconHandler}
                 className="cursor-pointer w-[27px]"
                 size={25}
               />
@@ -77,23 +103,71 @@ function GroupList() {
 
       {show ? (
         <div className="flex flex-col items-center h-full">
-          <form className="flex flex-col justify-center items-center w-full gap-[40px]">
+          <div className="flex flex-col justify-center items-center w-full gap-[40px]">
             <img
               src={random_profile}
               alt="#profile_pic"
               className="h-[50px] w-[50px] md:h-[100px] md:w-[100px] rounded-full object-cover cursor-pointer"
             />
             <TextField
+              className={shakeGroupName ? "shake" : ""}
               label="Group Name"
               variant="standard"
+              onChange={handleGroupName}
+              // value={groupName}
               type="text"
+              helperText={groupNameErr}
+              error={!!groupNameErr}
+              onAnimationEnd={handleAnimationEnd}
+              // sx={{
+              //   width: "368px",
+              //   "& .MuiInput-root": {
+              //     borderRadius: "8.6px",
+              //     "& fieldset": {
+              //       borderWidth: "2px",
+              //       opacity: 0.8,
+              //     },
+              //     "& input": {
+              //       fontSize: "20px",
+              //       color: "#11175D",
+              //     },
+              //   },
+              //   "& label": {
+              //     fontSize: "19px",
+              //     top: "-5px",
+              //   },
+              //   "& label.MuiInputLabel-shrink": {
+              //     fontSize: "16px",
+              //     backgroundColor: "#fff",
+              //     letterSpacing: "3px",
+              //   },
+              // }}
               sx={{
                 width: "368px",
                 "& .MuiInput-root": {
+                  // height: "81px",
                   borderRadius: "8.6px",
                   "& fieldset": {
+                    borderColor: groupNameErr ? "#FF0000" : "#11175D4D",
                     borderWidth: "2px",
                     opacity: 0.8,
+                  },
+                  "&:after": {
+                    borderBottom: `2px solid ${
+                      groupNameErr ? "#FF0000" : "#11175D4D"
+                    }`,
+                  },
+                  "&:hover:before": {
+                    borderBottomColor: groupNameErr ? "#FF0000" : "#11175D4D",
+                  },
+                  "&:hover fieldset": {
+                    borderColor: groupNameErr ? "#FF0000" : "#11175D4D",
+                  },
+                  "&.Mui-focused fieldset": {
+                    borderColor: groupNameErr ? "#FF0000" : "#11175D4D",
+                  },
+                  "& input::placeholder": {
+                    color: groupNameErr ? "#FF0000" : "#999",
                   },
                   "& input": {
                     fontSize: "20px",
@@ -103,18 +177,24 @@ function GroupList() {
                 "& label": {
                   fontSize: "19px",
                   top: "-5px",
+                  color: groupNameErr ? "#FF0000" : "#11175D",
                 },
                 "& label.MuiInputLabel-shrink": {
                   fontSize: "16px",
+                  color: groupNameErr ? "#FF0000" : "#11175D",
                   backgroundColor: "#fff",
                   letterSpacing: "3px",
                 },
               }}
             />
-            <button className="bg-[#3D77BE] hover:bg-[#3164A5] text-white px-4 py-2 rounded font-semibold cursor-pointer font-regular">
+            {/* <p>{groupNameErr}</p> */}
+            <button
+              className="bg-[#3D77BE] hover:bg-[#3164A5] text-white px-4 py-2 rounded font-semibold cursor-pointer font-regular"
+              onClick={handleCreateGroup}
+            >
               Create Group
             </button>
-          </form>
+          </div>
         </div>
       ) : (
         <div className="overflow-y-scroll h-[94%] pr-2">
